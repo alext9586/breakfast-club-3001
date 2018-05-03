d389c38b10818269ed82736b0cc0ef9700425eb6
Alex Tam <alex.tam@vauto.com>
2018-05-03 10:23:31 -0500

    Updated the datetime picker

--- client/src/Components/ArrivalForm/ArrivalFormContainer.tsx
+++ client/src/Components/ArrivalForm/ArrivalFormContainer.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
-import DateTimePicker from 'react-datetime-picker';
+import DatePicker from 'react-datepicker';
+import * as moment from 'moment';
 import { IMember } from 'src/Models/Member';
 import { Arrival } from 'src/Models/Arrival';
 import { IArrivalSend } from 'src/Models/RawViewModels';
+
+import 'react-datepicker/dist/react-datepicker.css';
+import './ArrivalForm.css';
+import { Moment } from 'moment';
 
 interface IArrivalFormContainerProps {
     member: IMember;
@@ -12,25 +17,25 @@ interface IArrivalFormContainerProps {
 }
 
 interface IArrivalFormContainerStates {
-    arrivalTime: Date;
+    arrivalTime: Moment;
     notes: string;
 }
 
 export class ArrivalFormContainer extends React.Component<IArrivalFormContainerProps, IArrivalFormContainerStates> {
-    private formStyle = {
-        height: "500px"
-    };
-
     constructor(props: IArrivalFormContainerProps) {
         super(props);
 
         this.state = {
-            arrivalTime: new Date(),
+            arrivalTime: moment(),
             notes: ""
         }
+
+        this.handleTimeChange = this.handleTimeChange.bind(this);
+        this.handleNotesChange = this.handleNotesChange.bind(this);
+        this.saveAction = this.saveAction.bind(this);
     }
 
-    private handleTimeChange(date: Date): void {
+    private handleTimeChange(date: Moment): void {
         this.setState({
             arrivalTime: date,
             notes: this.state.notes
@@ -47,49 +52,68 @@ export class ArrivalFormContainer extends React.Component<IArrivalFormContainerP
     private saveAction(): void {
         const data: IArrivalSend = {
             memberId: this.props.member.id,
-            arrivalTime: this.state.arrivalTime,
+            arrivalTime: this.state.arrivalTime.toDate(),
             notes: this.state.notes
         };
 
         this.props.saveAction(data);
     }
 
+    private renderDatePicker(): JSX.Element {
+        return (
+            <div className="form-group">
+                <label>Arrival Time</label>
+                <div>
+                    <DatePicker
+                        selected={this.state.arrivalTime}
+                        onChange={this.handleTimeChange}
+                        inline
+                        showTimeSelect
+                        timeIntervals={15}
+                        dateFormat="LLL"
+                        timeCaption="Time"
+                        />
+                </div>
+            </div>
+        );
+    }
+
+    private renderNotes(): JSX.Element {
+        return (
+            <div className="form-group">
+                <label htmlFor="arrivalNotes">Notes</label>
+                <input type="text"
+                    className="form-control"
+                    name="arrivalNotes"
+                    value={this.state.notes}
+                    onChange={this.handleNotesChange} />
+            </div>
+        );
+    }
+
     render(): JSX.Element {
         const {cancelAction} = this.props;
         const fullname = `${this.props.member.firstName} ${this.props.member.lastName}`;
-        const handleTimeChange = this.handleTimeChange.bind(this);
-        const handleNotesChange = this.handleNotesChange.bind(this);
-        const saveAction = this.saveAction.bind(this);
 
         return (
-            <form style={this.formStyle}>
+            <form>
                 <h1>Member Arrival</h1>
                 <h2>{fullname}</h2>
-                <div className="form-group">
-                    <label>Arrival Time</label>
-                    <div>
-                        <DateTimePicker
-                            onChange={handleTimeChange}
-                            value={this.state.arrivalTime} />
+                <div className="row">
+                    <div className="col-lg-5">
+                        { this.renderDatePicker() }
                     </div>
-                </div>
-                <div className="form-group">
-                    <div className="form-group">
-                        <label htmlFor="arrivalNotes">Notes</label>
-                        <input type="text"
-                            className="form-control"
-                            name="arrivalNotes"
-                            value={this.state.notes}
-                            onChange={handleNotesChange} />
+                    <div className="col-lg-7">
+                        { this.renderNotes() }
+                        <div className="form-group text-right">
+                            <button type="button" className="btn btn-outline-secondary" onClick={e => cancelAction()}>
+                                Cancel
+                            </button>
+                            <button type="button" className="btn btn-primary" onClick={this.saveAction}>
+                                Save and Rotate
+                            </button>
+                        </div>
                     </div>
-                </div>
-                <div className="form-group text-right">
-                    <button type="button" className="btn btn-outline-secondary" onClick={e => cancelAction()}>
-                        Cancel
-                    </button>
-                    <button type="button" className="btn btn-primary" onClick={saveAction}>
-                        Save and Rotate
-                    </button>
                 </div>
             </form>
         );
